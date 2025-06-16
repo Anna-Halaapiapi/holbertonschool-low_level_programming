c1ed213ce3f389d8154b0cc1337913a4888085e0
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-16 06:34:00 +0000

    Add 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ * Description: Prints string using puts function
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
