a0472367bd2e3aa484b09c3ab12576acce1a5561
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-17 05:34:16 +0000

    Add 1-last_digit.c

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Prints last digit of n and relevant string
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+	/* create local variable using modulus for last digit*/
+	int lastdigit = n % 10;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	/* prints string with value of n and last digit */
+	printf("Last digit of %d is %d", n, lastdigit);
+
+	/* prints if last digit of n is greater than 5 */
+	if (lastdigit > 5)
+		printf("and is greater than 5/n");
+	/* prints if last digit of n is zero */
+	else if (lastdigit == 0)
+		printf("and is 0/n");
+	/* prints if last digit of n is less than 6 but not 0 */
+	else if ((lastdigit < 6) && (lastdigit != 0))
+		printf("and is less than 6 and not 0/n");
+	return (0);
+}
