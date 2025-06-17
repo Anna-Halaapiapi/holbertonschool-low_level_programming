601b4e0fffed72e3b7552f37be4baa8636412adf
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-17 06:24:43 +0000

    Add 2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Prints alphabet in lower case
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
