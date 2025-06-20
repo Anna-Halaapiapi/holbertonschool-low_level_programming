381883387283d7363785aadf1b258de81d38f4a3
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-20 12:13:13 +0000

    Add 11-print_to_98.c

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - prints numbers from var n to 98
+ * @n: stores number to test
+ * Return: displays all natural numbers from n to 98
+ */
+
+void print_to_98(int n)
+{
+	while (n != 98)
+	{
+/* scenario - n is greater than 98 */
+	if (n > 98)
+	{
+		printf("%d, ", n);
+		n = n - 1;
+	}
+/* scenario - is less than 98 */
+
+	else if (n < 98)
+	{
+		printf("%d, ", n);
+		n = n + 1;
+	}
+	}
+/* print 98 */
+	printf("98");
+}
