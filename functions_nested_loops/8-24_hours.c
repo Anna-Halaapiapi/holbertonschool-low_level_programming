6efb10aab9fa032bf130b848582ee8ef74c4c0a0
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-20 06:53:06 +0000

    Add 8-24_hours.c

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * jack_bauer - prints all minutes of a day
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+
+/* outer hr loop */
+	while(h < 24)
+	{
+/* reset mins at start of each hr */
+		int m = 0;
+/* inner min loop */
+		while(m < 60)
+		{
+			_putchar('0' + h / 10);
+			_putchar('0' + h % 10);
+			_putchar(':');
+			_putchar('0' + m / 10);
+			_putchar('0' + m % 10);
+			_putchar('\n');
+			m = m + 1;
+		}
+			h = h + 1;
+		}
+
+}
