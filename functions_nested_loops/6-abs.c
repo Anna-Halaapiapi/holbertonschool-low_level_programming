4a96e41890aa058ae3396e2dd0174b4298778c93
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-20 02:52:35 +0000

    Add 6-abs.c

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value of an integer
+ * @n: number to check
+ * Return: abs of n
+ */
+
+int _abs(int n)
+
+{
+	if (n < 0)
+		return (-n);
+
+	else
+
+		return (n);
+}
