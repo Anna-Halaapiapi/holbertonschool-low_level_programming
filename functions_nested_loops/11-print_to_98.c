b06df6b9c0f7f5d67fc281a8d102f43015ade9e9
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-20 12:15:15 +0000

    Add 11-print_to_98.c

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -14,17 +14,17 @@ void print_to_98(int n)
 /* scenario - n is greater than 98 */
 	if (n > 98)
 	{
-		printf("%d, ", n);
+		printf("%d, \n", n);
 		n = n - 1;
 	}
 /* scenario - is less than 98 */
 
 	else if (n < 98)
 	{
-		printf("%d, ", n);
+		printf("%d, \n", n);
 		n = n + 1;
 	}
 	}
 /* print 98 */
-	printf("98");
+	printf("98\n");
 }
