ee65a72b0fe78098235bf3bc32d4c06849135655
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-23 07:25:05 +0000

    Add 9-fizz_buzz.c

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description: prints numbers 1-100
+ * Return: void
+ */
+
+int main(void)
+{
+
+	int n = 1;
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+		n = n + 1;
+	}
+	putchar('\n');
+}
