1b71679643d224d96dc313928a8aac32dd7e5b36
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-23 03:16:59 +0000

    Add 5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0-14
+ *
+ * Return: prints numbers 0-14, new line, over 10 rows
+ */
+
+void more_numbers(void)
+{
+	int r = 1;
+
+/* outer loop for 10 rows */
+while (r <= 10)
+{
+	int c = 0;
+
+/* inner loop for cols */
+	while (c <= 14)
+	{
+		_putchar('0' + c / 10);
+		_putchar('0' + c % 10);
+		c = c + 1;
+	}
+
+	_putchar('\n');
+	r = r + 1;
+}
+}
