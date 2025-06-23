e139e89663612e8e59c36687b40e18a7884a5449
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-23 02:02:24 +0000

    Add 1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit (0-9)
+ * @c: character for checking
+ * Return: return 1 if c is digit, else return 0
+ */
+
+int _isdigit(int c)
+{
+
+	if (_isdigit(c))
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+
+}
