768f6768e4897df615dda4f2467ba3996529581b
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-23 08:16:01 +0000

    Add 10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle using #
+ * @size: size of triangle
+ * Return: triangle using #
+ */
+
+void print_triangle(int size)
+{
+	int count = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+/* outer loop for rows/size */
+	while (count < size)
+	{
+		int width = 0;
+
+
+/* inner loop for columns for spaces and '#' */
+		while(width < size - count - 1)
+		{
+			_putchar(' ');
+			width = width + 1;
+		}
+
+		width = 0;
+
+		while (width < count + 1)
+		{
+			_putchar('#');
+			width = width + 1;
+		}
+		_putchar('\n');
+		count = count + 1;
+	}
+
+}
