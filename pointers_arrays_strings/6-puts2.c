900321cb61253c672017f0a5359d3a5e420c2c8c
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-24 07:43:02 +0000

    Add 6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ * @str: pointer to string str
+ * Return: void
+ */
+
+void puts2(char *str)
+
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str = str + 2;
+	}
+	_putchar('\n');
+}
