441ef7979b3ed6fd545e70039956984e5e53cad3
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-24 08:04:58 +0000

    Add 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,15 @@ void puts2(char *str)
 	while (*str != '\0')
 	{
 		_putchar(*str);
-		str = str + 2;
+/* move fwd 1 character at a time to check if it's null term */
+		str = str + 1;
+		if (*str == '\0')
+		{
+			break;
+		}
+		else
+		{
+			str = str + 1;
+		}
 	}
-	_putchar('\n');
 }
