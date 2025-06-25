270e385a13370e005fd9476cecf3c741cc172496
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-06-25 04:05:17 +0000

    Add 8-print_array.c

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - prints all elements of an int array
+ * @a: pointer to int a
+ * @n: number of ints in array
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+
+{
+/* loop counter var */
+	int i = 0;
+
+	while (i < n)
+	{
+	printf("%d", a[i]);
+/* print comma and space if not last int */
+	if (i != n - 1)
+	{
+		printf(", ");
+	}
+	i = i + 1;
+	}
+	_putchar('\n');
+}
