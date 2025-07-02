fbacc17c8df03e5821fc67dc75b57993d966647a
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-02 11:54:48 +0000

    Add 3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * factorial - returns factorial of a number
+ * @n: number
+ * Return: factorial or -1 if n less than 0
+ */
+
+int factorial(int n)
+
+{
+	if (n < 0)
+	{
+		return(-1);
+	}
+	if (n == 1 || n == 0)
+	{
+		return(1);
+	}
+	return n * factorial(n - 1)!;
+}
