4238e41caede98fb809d2cbd44e701b7139a8ca2
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-03 05:32:22 +0000

    Add 4-pow_recursion.c

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -16,7 +16,7 @@ int _pow_recursion(int x, int y)
 	{
 		return (-1);
 	}
-/* edge case */
+/* base case */
 	if (y == 0)
 	{
 		return (result);
@@ -24,8 +24,7 @@ int _pow_recursion(int x, int y)
 
 	else
 	{
-		result = _pow_recursion(x, y - 1);
+		return (x * _pow_recursion(x, y - 1));
 	}
 
-	return (result);
 }
