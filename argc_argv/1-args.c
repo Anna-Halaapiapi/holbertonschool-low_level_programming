0a40accd139be2f4c5641f3450e22b8d312f6e33
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-04 07:41:02 +0000

    Add 1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program entry point
+ * @argc: argument count (unused)
+ * @argv: argument vector - array of pointers to strings
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	void(argv);
+	printf("%d\n", argc);
+	return (0);
+}
