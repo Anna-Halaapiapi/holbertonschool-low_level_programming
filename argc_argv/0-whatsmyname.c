4cf03789c6f6d9fee4335a17e3dddbb8ccc07501
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-04 07:21:34 +0000

    Add 0-whatsmyname.c, directory and README.md for directory

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argv: argument vector - pointer to array of strings
+ * Return: 0 (success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
