cc5cc9cdebf6c1b0d79e3f8b56eb5d3047e661bf
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-07 06:29:43 +0000

    Add 1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stddef.h> /* for NULL */
 #include <stdlib.h> /* for malloc & free */
-#include <string.h> /* for strlen & strcpy */
+#include <string.h> /* for strlen */
 
 /**
  * _strdup - duplicates string
@@ -14,6 +14,9 @@ char *_strdup(char *str)
 /* declare var to hold a copy of str */
 	char *copyofstr;
 
+/* declare var to keep track of index position */
+	unsigned int index = 0;
+
 /* return NULL if str is NULL */
 	if (str == NULL)
 	{
@@ -23,19 +26,26 @@ char *_strdup(char *str)
 /* allocate memory for the copy of string */
 	copyofstr = (char *)malloc(strlen(str) + 1);
 
+/* loop through to copy str into copystr */
+	while (str != '\0')
+	{
+
 /* check for successful allocation and copy str into copyofstr */
 	if (copyofstr != NULL)
 	{
-		strcpy(copyofstr, str);
+		copyofstr[index] = str[index];
 	}
 /* if allocattion unsuccessful, return NULL */
 	else
 	{
 		return (NULL);
 	}
+
+	index = index + 1;
+	}
 /* return pointer to copyofstr */
 	return (copyofstr);
 
 /* free new string */
-	free(copyofstr);
+	free(str);
 }
