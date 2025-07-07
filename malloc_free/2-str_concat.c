99bc5eb6dd342b8e6e0247a93c3192123f35b4ea
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-07 10:21:49 +0000

    Add 2-str_concat.c

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -9,50 +9,31 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int s1length = 0; /* keep track of s1 length */
-	int s2length = 0; /* keep track of s2 length */
-	int index = 0; /* keep track of index position */
-	char *s1start = s1; /* keep track of s1 start address */
-	char *s2start = s2; /* keep track of s2 start address */
-	char *final; /* for memory allocation */
-	int totallength; /* for s1 + s2 */
+	int s1length = 0, s2length = 0, index, totallength;
+	char *s1start = s1, *s2start = s2, *final;
 
 	if (s1 == NULL) /* NULL to be treated as empty strings */
 	s1 = "";
 	if (s2 == NULL)
-		s2 = "";
-	while (*s1 != '\0') /* find length of s1 */
-	{
-		s1length = s1length + 1;
-		s1 = s1 + 1;
-	}
+	s2 = "";
+	for (; *s1 != '\0'; s1++) /* find length of s1 */
+	s1length = s1length + 1;
+	for (; *s2 != '\0'; s2++) /* find length of s2 */
+	s2length = s2length + 1;
 
-	while (*s2 != '\0') /* find length of s2 */
-	{
-		s2length = s2length + 1;
-		s2 = s2 + 1;
-	}
 	totallength = s1length + s2length;
 	final = malloc(totallength + 1); /* malloc both lengths + null term */
 
 	if (final == NULL) /* if malloc unsuccessful - return NULL */
-	{
-		return (NULL);
-	}
+	return (NULL);
+
 	s1 = s1start; /* reset s1 and s2 pointers to their starts */
 	s2 = s2start;
 
-	while (s1[index] != '\0') /* copy s1 to final */
-	{
-		final[index] = s1[index];
-		index = index + 1;
-	}
-	index = 0; /* reset index */
-	while (s2[index] != '\0') /* copy s2 to end of s1 in final */
-	{
-		final[s1length + index] = s2[index];
-		index = index + 1;
-	}
+	for (index = 0; s1[index] != '\0'; index++) /* copy s1 to final */
+	final[index] = s1[index];
+	for (index = 0; s2[index] != '\0'; index++) /* copy s2 to end of s1 */
+	final[s1length + index] = s2[index];
 	final[totallength] = '\0'; /* add null term to end */
 	return (final); /* return final */
 }
