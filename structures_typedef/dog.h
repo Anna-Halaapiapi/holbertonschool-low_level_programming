2168346bd898fff1430381797112b83981234345
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-10 07:01:10 +0000

    Add dog.h for task 3

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,6 +3,7 @@
 
 /**
  * struct dog - structure with name, age, owner
+ * dog_t - new name for type struct dog
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
@@ -13,12 +14,11 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-};
+}
+
+	typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-
-
 
 #endif /* _dog_h_ */
