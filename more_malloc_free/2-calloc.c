4f06067821fdc1a2c0ad7a713402816290d3eda1
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-10 01:20:56 +0000

    Add 2-calloc.c

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -12,8 +12,8 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 /* vars */
-	unsigned int *memory; /* pointer to memory */
-	unsigned int index;
+	char *memory; /* pointer to memory */
+	unsigned int index = 0;
 
 /* return NULL if nmemb or size is 0 */
 	if (nmemb == 0 || size == 0)
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 
 /* malloc the array */
-	memory = malloc(nmemb * sizeof(size));
+	memory = malloc(nmemb * size);
 
 /* check if malloc fails */
 	if (memory == NULL)
