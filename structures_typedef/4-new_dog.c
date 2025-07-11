1005b7d7aaf7f37f4c4cbf44c769df6ed7294f17
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-11 00:48:15 +0000

    Add 4-new_dog.c

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,27 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stddef.h> /* for NULL */
+
+/**
+ * new_dog - creates a new dog
+ * @name: pointer to dog name
+ * @age: age of dog
+ * @owner: pointer to dog owner
+ * Return: NULL if function fails
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+/* declare var of type dog_t */
+	dog_t dog1;
+
+/* return NULL */
+	if (dog_t == NULL)
+	{
+		return (NULL);
+	}
+/* assign values to members */
+	dog1->name = name;
+	dog1->age = age;
+	dog1->owner = owner;
+}
