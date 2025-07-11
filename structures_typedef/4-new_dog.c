9d05086d8c86684309e25ff8653865a3cd5666e8
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-11 03:03:41 +0000

    Add 4-new_dog.c

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stddef.h> /* for NULL */
+#include <stdlib.h> /* for malloc & free */
 
 /**
  * new_dog - creates a new dog
@@ -12,13 +13,44 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-/* declare var of type dog_t */
-	dog_t dog1;
+	int namelength, ownerlength, totalstringlength, i, j;
+	char *stringmemory;
+	dog_t *dog1;
 
-/* assign values to members */
-	dog1.name = name;
-	dog1.age = age;
-	dog1.owner = owner;
+	dog1 = (dog_t *)malloc(sizeof(dog_t)); /* malloc struct instance */
+
+	if (dog1 == NULL) /* check if malloc failed */
+	{
+		return (NULL);
+	}
+
+	namelength = strlen(name); /* find length of name string */
+	ownerlength = strlen(owner); /* find length of owner string */
+	totallength = namelength + ownerlength + 2; /* calc total length */
+
+	stringmemory = malloc(totallength); /* malloc for strings */
+
+	if (stringmemory == NULL) /* check if malloc failed */
+	{
+		return (NULL);
+	}
+
+	while (name[j] != '\0') /* copy name to heap memory */
+	{
+		stringmemory[i] = name[j];
+		i++;
+		j++;
+	}
+	stringmemory[i] = '\0';
+	j = 0;
+
+	while (owner[j] != '\0') /* copy owner to heap memory */
+	{
+		stringmemory[i] = owner[j];
+		i++;
+		j++;
+	}
+	stringmemory[i] = '\0';
 
 	return (dog1);
 }
