b9a56c6970ce26abad652d3690d91804d2136983
Anna-Halaapiap <annahalaapiapi@hotmail.com>
2025-07-14 04:41:12 +0000

    Update function_pointers.h

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
-/* custom header file */
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
+
+/* custom header file */
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
