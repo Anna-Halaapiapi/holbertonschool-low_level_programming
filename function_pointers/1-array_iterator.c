d5936e4496166c12df106d7d693aae4fddd67b5e
Anna-Halaapiap <annahalaapiapi@hotmail.com>
2025-07-14 07:05:11 +0000

    Update 1-array_iterator.c

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,8 +1,9 @@
 #include "function_pointers.h"
 #include <stdlib.h> /* for malloc, free, exit */
+#include <stddef.h> /* for size_t */
 
 /**
- * array_iterator - executes a function given as a parameter on each element of array
+ * array_iterator - execute function given as parameter on each element of arr
  * @array: pointer to array
  * @size: size of array
  * @action: pointer to function to use
@@ -11,7 +12,7 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int index = 0; /* keep track of arr index position */
+	unsigned int index = 0; /* keep track of arr index position */
 
 	while (index < size)/*loop through each elem of arr and execute action*/
 	{
