1d8e7e73f19aa493d610dc5c60f06a42f61297f3
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-15 01:02:16 +0000

    Add 2-int_index.c

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+#include <stdlib.h> /* for malloc, free, exit, NULL */
+
+
+/**
+ * int_index - searches for an integer
+ * @array: array
+ * @size: size of array
+ * @cmp: compares integers
+ * Return: index of matching int, or -1
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int index = 0;
+
+	if (size <= 0 || cmp == NULL || array == NULL)
+	{
+		return (-1);
+	}
+	while (index < size)
+	{
+	cmp(array[index]);
+	if (cmp(array[index]) == 1
+	{
+	return (index);
+	}
+	index = index + 1;
+	}
+	return (-1);
+}
