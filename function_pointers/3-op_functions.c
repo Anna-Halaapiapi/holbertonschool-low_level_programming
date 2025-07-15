73edf97581a8adb0c14461707b4f031615ae2888
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-15 01:38:25 +0000

    Add 3-op_functions.c

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include "function_pointers.h"
+#include <stdlib.h> /* for malloc, free, exit, NULL */
+#include <stdio.h>
+
+/**
+ * op_add - adds two numbers
+ * @a: number1
+ * @b: number2
+ * Return: int result of operation
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract num2 from num1
+ * @a: number1
+ * @b: number2
+ * Return: int result of operation
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies num1 and num2
+ * @a: number1
+ * @b: number2
+ * Return: int result of operation
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides num1 by num2
+ * @a: number1
+ * @b: number2
+ * Return: int result of operation
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculates remainder of a divided by b
+ * @a: number1
+ * @b: number2
+ * Return: int result of operation
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
