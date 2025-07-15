4a3b7ffe7184ca8c588ca5f5ae077962f333393e
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-15 04:22:35 +0000

    Add incomplete main file

---
+++ function_pointers/3-main.c
@@ -0,0 +1,35 @@
+#include "calc.h"
+#include <stdlib.h> /* for malloc, free, exit, NULL */
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: result of operation
+ * OR Error (status 98) - if num arguments is wrong
+ * OR Error (status 99) - if operator is not +,-,*,/,%
+ * OR Error (status 100) - if user divides (/ or %) by 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+/* status 98 */
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+/* status 99 */
+	if (argv[2] != )
+	{
+		printf("Error\n");
+		exit(99);
+	}
+/* status 100 */
+	if (argv[1] == 0 || argv[3] == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+}
