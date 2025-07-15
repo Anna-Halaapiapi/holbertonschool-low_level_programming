9271bd05fe9f2378034c3a51b2fb98705f24d9b9
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-15 05:14:32 +0000

    Updating 3-get_op_func.c for betty compliance

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -27,7 +27,7 @@ int (*get_op_func(char *s))(int, int)
 		if (strcmp(s, ops[i].op) == 0)
 	{
 		/* return the function from ops array index position */
-		return(ops[i].f);
+		return (ops[i].f);
 	}
 
 	i = i + 1;
