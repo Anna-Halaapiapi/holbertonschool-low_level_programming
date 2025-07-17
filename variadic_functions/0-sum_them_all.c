eb41bc43cf2ef84f05546052b907789c915b8c79
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-17 00:26:18 +0000

    Add 0-sum_them_all.c

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,16 +2,22 @@
 #include <stdlib.h> /* for malloc, free & exit */
 #include <stdarg.h> /* for va_start, va_arg, and va_end */
 
+/**
+ * sum_them_all - function that calculates sum of all parameters
+ * @n: number of parameters
+ * Return: int sum of all parameters
+ */
+
 int sum_them_all(const unsigned int n, ...)
 {
 	int sum = 0;
 	unsigned int index = 0;
+	va_list list;
 
 	if (n == 0)
 	{
 		return (0);
 	}
-	va_list list;
 
 	while (index < n)
 	{
