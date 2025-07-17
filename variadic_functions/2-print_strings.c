fd3768323f08fd658a30a5934b9cae7bdce9f8e1
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-17 03:48:44 +0000

    Add 2-print_strings.c

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,44 @@
+#include "variadic_functions.h"
+#include <stdlib.h> /* for malloc, free & exit */
+#include <stdarg.h> /* for va_start, va_arg, and va_end */
+#include <stdio.h> /* for printf */
+
+/**
+ * print_strings - prints strings, followed by a new line.
+ * @separator: the string to be printed between the strings.
+ * @n: number of strings passed to the function.
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	int index = 0;
+	char *string; /* temp pointer to hold va_args value */
+	va_list list;
+
+	va_start(list, n);
+
+	if (separator == NULL)
+	{
+		separator = "";
+	}
+
+	while (index < (n - 1))
+	{
+		string = va_args(list, char *);
+
+		if (string == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("%s", string);
+		}
+
+		index = index + 1;
+	}
+	printf("%s\n", va_args(list, char *));
+
+	va_end(list);
+}
