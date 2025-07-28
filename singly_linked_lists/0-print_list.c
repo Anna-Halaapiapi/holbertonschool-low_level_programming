dffc88bf89719d49adc9911c5c66f78fe5410b7b
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-28 06:36:32 +0000

    Add 0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all elements of list_t list
+ * @h: pointer to head of list_t list
+ *
+ * Return: number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count = 0; /* keeps track of number of nodes */
+
+/* loop through list and print */
+	while (h != NULL)
+	{
+
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)");
+		}
+
+		else
+		{
+			printf("&u %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+
+/* return number of nodes in list */
+	return (count);
+}
