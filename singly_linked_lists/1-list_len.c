34e3ded090ea06add17f7413e9dcd41ee221639f
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-28 07:05:24 +0000

    Update 1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -11,7 +11,7 @@
 
 size_t list_len(const list_t *h)
 {
-	unsigned int num; /* keep track of number of elements */
+	unsigned int num = 0; /* keep track of number of elements */
 
 	while (h != NULL)
 	{
