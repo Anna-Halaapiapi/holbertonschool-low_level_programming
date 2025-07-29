f05fa8cf2582e40aa250a8aff639a95a0f815941
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-29 07:24:20 +0000

    Add 4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h> /* for malloc, NULL */
+
+/**
+ * free_list - function that frees a list
+ * @head: pointer to head of list
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head; /* to store current node */
+	list_t *nextnode;/* to store next node */
+
+	while (current != NULL)
+	{
+		nextnode = current->next; /* save pointer to next node */
+		free(current); /* free current node */
+		current = nextnode; /* move to next node */
+	}
+
+	*head = NULL; /* set head to NULL after freeing all nodes */
+}
