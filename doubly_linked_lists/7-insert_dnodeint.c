1842d32f4592472ddfa03605a30cdf6cfbbe555c
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-30 07:18:34 +0000

    Update 7-insert_dnodeint.c

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -14,17 +14,32 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-/* to traverse list */
-	unsigned int counter = 0;
-	dlistint_t *prevnode = *h; /* start at node 1 */
-	dlistint_t *nextnode;
+	unsigned int counter = 0; /* to traverse list */
+	dlistint_t *prevnode = *h; /* to store prevnode to newnode */
+	dlistint_t *nextnode; /* to store nextnode to newnode */
 	dlistint_t *newnode;
 
-/* call add_dnodeint to add node to start of list */
+/* call add_dnodeint to add node to start of list if required */
 	if (idx == 0)
 	{
 		return(add_dnodeint(h, n));
 	}
+
+/* traverse list to find prev node for newnode */
+	while (counter < (idx - 1) && prevnode != NULL)
+	{
+		prevnode = prevnode->next; /* move to next node */
+		counter = counter + 1;
+	}
+
+/* call add_dnodeint_end to add node to end of list if required */
+	if (prevnode->next == NULL)
+	{
+		return(add_dnodeint_end(h, n));
+	}
+
+/* find next node for newnode */
+	nextnode = prevnode->next;
 
 /* malloc newnode and check for failure */
 	newnode = malloc(sizeof(dlistint_t));
@@ -36,22 +51,6 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 /* assign n to newnode */
 	newnode->n = n;
-
-/* traverse list to find prev node for newnode */
-	while (counter < (idx - 1) && prevnode != NULL)
-	{
-		prevnode = prevnode->next; /* move to next node */
-		counter = counter + 1;
-	}
-
-/* find next node for newnode */
-	nextnode = prevnode->next;
-
-/* call add_dnodeint_end to add node to end of list */
-	if (nextnode == NULL)
-	{
-		return(add_dnodeint_end(h, n));
-	}
 
 /* update all pointers to insert newnode */
 	prevnode->next = newnode;
