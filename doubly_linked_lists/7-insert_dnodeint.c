ca83c9c59a5f2ff11fcfc63e60401f8ac55087b2
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-07-30 06:50:46 +0000

    Add 7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
+
+/**
+ * insert_dnodeint_at_index - inserts node at a given position
+ * @h: double pointer to head
+ * @idx: index of list where new node should be inserted (starts at 0)
+ * @n: integer data for new node
+ *
+ * Return: address of newnode or NULL (failure)
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+/* to traverse list */
+	unsigned int counter = 0;
+	dlistint_t *prevnode = h; /* start at node 1 */
+	dlistint_t *nextnode;
+
+/* malloc newnode and check for failure */
+	dlistint_t *newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+	{
+		return (NULL);
+	}
+
+/* assign n to newnode */
+	newnode->n = n;
+
+/* traverse list to find prev node for newnode */
+	while (counter < idx && prevnode != NULL)
+	{
+		prevnode = prevnode->next; /* move to next node */
+		counter = counter + 1;
+	}
+
+/* find next node for newnode */
+	nextnode = prevnode->next;
+
+/* update all pointers to insert newnode */
+	prevnode->next = newnode;
+	nextnode->prev = newnode;
+	newnode->prev = prevnode;
+	newnode->next = nextnode;
+
+	return (newnode);
+}
