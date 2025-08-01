4b8db2dcd12e089faedaef3f5876392620c51e61
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-01 06:51:18 +0000

    Update files

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,6 +4,19 @@
 #include <fcntl.h> /* for open */
 
 /**
+ * __putchar - writes the character c to stderr
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+int _putchar(char c)
+{
+	return (write(2, &c, 1));
+}
+
+/**
  * read_textfile - reads a txt file and prints it to POSIX stdout
  * @filename: pointer to file to read and print
  * @letters: number of letters to read and print
@@ -14,7 +27,7 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t bytesread; /* keep track of bytes read */
-	char buffer[20000]; /* buffer to hold read contents of file */
+	char buffer[10000]; /* buffer to hold read contents of file */
 	int i = 0; /* track position in buffer */
 	int fd; /* file descriptor */
 
@@ -39,7 +52,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	while (i < bytesread) /* loop through chars in buffer */
 	{
-		if (_putchar(buffer[i]) == -1)/* print to stdout & check fail */
+		if (__putchar(buffer[i]) == -1)/* print to stdout & check fail */
 		{
 			close(fd);
 			return (0);
