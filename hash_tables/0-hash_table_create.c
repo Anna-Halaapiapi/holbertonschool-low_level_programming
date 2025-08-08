261a5bdc02eafdf425c88bbb4f5cb0058a43ad6c
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-08 01:30:44 +0000

    add 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -11,15 +11,32 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t new_table;
+	hash_table_t *new_table; /* pointer to hash table */
+	unsigned long int i = 0; /* loop counter */
 
-	if (size == NULL)
+	if (size == 0) /* check size */
 		return (NULL);
 
-	new_table = malloc(sizeof(hash_table_t));
+	new_table = malloc(sizeof(hash_table_t)); /* malloc new table */
 
-	if (new_table == NULL)
+	if (new_table == NULL) /* handle failure */
 		return (NULL);
 
-	return (new_table);
+	/* malloc for nodes */
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+
+	if (new_table->array == NULL) /* handle failure */
+	{
+		free(new_table);
+		return (NULL);
+	}
+
+	/* initialise array at NULL */
+	while (i < size)
+	{
+		new_table->array[i] = NULL;
+		i++;
+	}
+
+	return (new_table); /* pointer to new table */
 }
