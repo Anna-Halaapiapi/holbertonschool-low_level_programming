218cc4487d18874de9b7b79ec42699ab538c434e
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-08 02:34:55 +0000

    add 2-key_index.c

---
+++ hash_tables/2-key_index.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * key_index - gives index of a key using hash_djb2
+ * @key: the key
+ * @size: size of arrae
+ *
+ * Return: index where key is stored in table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	/* index of key */
+	unsigned long int index = hash_djb2 (key) % size;
+
+	/* return index of key */
+	return (index);
+}
