6ff9b6db113db880ebd376c9e4f3b4dbfdf2be9f
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-08 01:32:23 +0000

    add 0-hash_table_create.c

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -23,7 +23,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	/* malloc for nodes */
-	new_table->array = malloc(sizeof(hash_node_t) * size);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (new_table->array == NULL) /* handle failure */
 	{
