dcedd2281c08527cd0f631d9bc1df2f5839998a7
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-26 07:47:45 +0000

    add 3-cp.c and update header

---
+++ file_io/3-cp.c
@@ -0,0 +1,100 @@
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* exit */
+#include <unistd.h> /* open */
+#include <fcntl.h> /* open flags */
+#include <sys/types.h> /* ssize_t */
+
+/**
+ * exit_97 - display error message and exit with code 97
+ *
+ * Return: void
+ */
+void exit_97(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+
+/**
+ * exit_98 - display error message and exit with code 98
+ * @arg: argument passed from main
+ *
+ * Return: void
+ */
+void exit_98(char *arg)
+{
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg);
+	exit(98);
+}
+
+/**
+ * exit_99 - display error message and exit with code 99
+ * @arg: argument passed from main
+ *
+ * Return: void
+ */
+void exit_99(char *arg)
+{
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg);
+	exit(99);
+}
+
+/**
+ * exit_100 - display error message and exit with code 100
+ * @fd: file descriptor passed from main
+ *
+ * Return: void
+ */
+void exit_100(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+
+/**
+ * main - entry point to program
+ * Description: program that copies content of a file to another file
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 (success) or 97, 98 ,99 , 100 (fail)
+ */
+
+int main(int argc, char *argv[])
+{
+	int from_fd;
+	int to_fd;
+	char buffer[1024];
+	ssize_t bytesread, byteswritten;
+
+	if (argc != 3)
+		exit_97();
+
+	from_fd = open(argv[1], O_RDONLY);
+	to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+
+	if (from_fd == -1)
+		exit_98(argv[1]);
+
+	if (to_fd == -1)
+		exit_99(argv[2]);
+
+	while ((bytesread = read(from_fd, buffer, sizeof(buffer))) > 0)
+	{
+		byteswritten = write(to_fd, buffer, bytesread);
+		if (byteswritten != bytesread)
+		{
+			exit_99(argv[2]);
+		}
+	}
+	if (bytesread == -1)
+		exit_98(argv[1]);
+
+	if (close(from_fd) == -1)
+		exit_100(from_fd);
+
+	if (close(to_fd) == -1)
+		exit_100(to_fd);
+
+	return (0);
+}
--- file_io/main.h
+++ file_io/main.h
@@ -9,4 +9,11 @@ ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
+int main(int argc, char *argv[]);
+/* helpers */
+	void exit_97(void);
+	void exit_98(char *arg);
+	void exit_99(char *arg);
+	void exit_100(int fd);
+
 #endif
