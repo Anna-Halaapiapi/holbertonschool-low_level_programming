d2096c2c5d7a4a1c304776a30fc1ebcd3189a6c6
Anna-Halaapiapi <annahalaapiapi@hotmail.com>
2025-08-27 07:49:58 +0000

    update 3-cp.c

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -18,23 +18,43 @@ void exit_97(void)
 /**
  * exit_98 - display error message and exit with code 98
  * @arg: argument passed from main
+ * @from_fd: file descriptor of from file to close (or -1 to skip)
+ * @to_fd: file descriptor of to file to close (or -1 to skip)
  *
  * Return: void
  */
-void exit_98(char *arg)
+void exit_98(char *arg, int from_fd, int to_fd)
 {
 	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg);
+	if (from_fd != -1)
+	{
+		close(from_fd);
+	}
+	if (to_fd != -1)
+	{
+		close(to_fd);
+	}
 	exit(98);
 }
 
 /**
  * exit_99 - display error message and exit with code 99
  * @arg: argument passed from main
+ * @from_fd: file descriptor of from file to close (or -1 to skip)
+ * @to_fd: file descriptor of to file to close (or -1 to skip)
  *
  * Return: void
  */
-void exit_99(char *arg)
+void exit_99(char *arg, int from_fd, int to_fd)
 {
+	if (from_fd != -1)
+	{
+		close(from_fd);
+	}
+	if (to_fd != -1)
+	{
+		close(to_fd);
+	}
 	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg);
 	exit(99);
 }
@@ -70,20 +90,19 @@ int main(int argc, char *argv[])
 		exit_97();
 
 	from_fd = open(argv[1], O_RDONLY);
-	to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	if (from_fd == -1)
-		exit_98(argv[1]);
+		exit_98(argv[1], -1, -1);
 
+	to_fd = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (to_fd == -1)
-		exit_99(argv[2]);
+		exit_99(argv[2], from_fd, -1);
 
 	while (1)
 	{
 		bytesread = read(from_fd, buffer, sizeof(buffer));
 		if (bytesread == -1)
 		{
-			exit_98(argv[1]);
+			exit_98(argv[1], from_fd, to_fd);
 		}
 		if (bytesread == 0)
 		{
@@ -93,7 +112,7 @@ int main(int argc, char *argv[])
 		byteswritten = write(to_fd, buffer, bytesread);
 		if (byteswritten != bytesread || byteswritten == -1)
 		{
-			exit_99(argv[2]);
+			exit_99(argv[2], from_fd, to_fd);
 		}
 	}
 	if (close(from_fd) == -1)
